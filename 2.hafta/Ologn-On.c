a3c39096912759fe2c31077f95e2c71f7881a78f
Muhammed Sert <sertm0104@gmail.com>
2017-03-03 12:45:24 +0200

    Add files via upload

---
+++ 2.hafta/Ologn-On.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int step=0;
+
+int pow(int x, int y){
+    int us=1;
+    int i;
+    for (i=0;i<y;i++){
+        us=us*x;
+    }
+    return us;
+}
+long LongPow(int x,int n)
+{
+    if(n==0) return 1;
+    if(n==1) return x;
+    if(n%2==0)
+        return LongPow(x*x,n/2);
+    else
+        return LongPow(x*x,n/2)*x;
+}
+
+int main()
+{
+
+   int taban=5;
+   int ust=3;
+
+   printf("\nsonuc:%d",pow(taban,ust));
+   printf("\nsonuc:%d",LongPow(taban,ust));
+
+
+
+    return 0;
+}
